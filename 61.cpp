897f68430ff8647ede84d82f490714f32f1091c4
rampoe <mail.rampoe@gmail.com>
2024-02-14 00:31:18 +0500

    sync

---
+++ 61.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+  int a, b, s1 = 0, s2 = 0;
+  for (int i = 0; i < 4; i++)
+  {
+    std::cin >> a >> b;
+    s1 += a;
+    s2 += b;
+  }
+  if (s1 > s2)
+    std::cout << 1;
+  else if (s2 > s1)
+    std::cout << 2;
+  else
+    std::cout << "DRAW";
+  return 0;
+}
