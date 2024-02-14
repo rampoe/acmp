562522cf2cccd7b7a2fa0b3b827db34dfd469224
rampoe <mail.rampoe@gmail.com>
2024-02-14 22:43:52 +0500

    892

--- 892.cpp
+++ 892.cpp
@@ -2,6 +2,32 @@
 
 int main()
 {
-  setlocale(LC_ALL, "RUS");
-  int return 0;
+  int n = 0;
+  std::cin >> n;
+  switch (n)
+  {
+  case 1:
+  case 2:
+  case 12:
+    std::cout << "Winter" << std::endl;
+    break;
+  case 3:
+  case 4:
+  case 5:
+    std::cout << "Spring" << std::endl;
+    break;
+  case 6:
+  case 7:
+  case 8:
+    std::cout << "Summer" << std::endl;
+    break;
+  case 9:
+  case 10:
+  case 11:
+    std::cout << "Autumn" << std::endl;
+    break;
+  default:
+    std::cout << "Error" << std::endl;
+  }
+  return 0;
 }
